bca803446d388a3683408a1dc5612aad8be90682
David Griver <dgriver@gmail.com>
2015-10-07 02:53:45 +0300

    changed more settings

--- stocksearch/stocksearch/settings/base.py
+++ stocksearch/stocksearch/settings/base.py
@@ -123,16 +123,3 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
--- stocksearch/stocksearch/settings/development.py
+++ stocksearch/stocksearch/settings/development.py
@@ -13,3 +13,17 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
