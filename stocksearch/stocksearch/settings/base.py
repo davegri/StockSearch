fa1be8425f12b1fc35bfc57b57de2d45a5bae454
David Griver <dgriver@gmail.com>
2015-10-07 05:41:11 -0400

    production changes

--- stocksearch/stocksearch/settings/base.py
+++ stocksearch/stocksearch/settings/base.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = (
     'django_extensions',
     'crawlers',
     'website',
-    'debug_toolbar',
     'watson'
 )
 
--- stocksearch/stocksearch/settings/production.py
+++ stocksearch/stocksearch/settings/production.py
@@ -15,9 +15,9 @@ DATABASES = {
 }
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.dirname(BASE_DIR, '/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
