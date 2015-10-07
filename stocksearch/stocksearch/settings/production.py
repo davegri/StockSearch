599fee6aac9b94548251c835bedb345e8a3803cf
David Griver <dgriver@gmail.com>
2015-10-07 12:01:53 +0300

    setting changes

--- stocksearch/stocksearch/settings/production.py
+++ stocksearch/stocksearch/settings/production.py
@@ -14,6 +14,10 @@ DATABASES = {
     }
 }
 
-
-STATIC_ROOT = os.path.dirname(BASE_DIR)
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.dirname(BASE_DIR, '/static')
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
+
