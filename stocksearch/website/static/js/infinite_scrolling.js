a49b66f76955c656e1c67be1416c74b5fda08e3c
David Griver <dgriver@gmail.com>
2015-10-08 02:05:37 +0300

    another quickfix

--- stocksearch/website/static/js/infinite_scrolling.js
+++ stocksearch/website/static/js/infinite_scrolling.js
@@ -45,6 +45,7 @@ $.ajaxSetup({
         bindEvents: function(){
             $(document).on('scroll', function(){
                 if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+                    this.$more_button.remove()
                     this.getImages();
                 }
             }.bind(this));
