c9b6f9b9821b5b4846a71b95ca2a135bff512e24
David Griver <dgriver@gmail.com>
2015-10-19 18:19:55 +0300

    meged migrations

---
+++ stocksearch/crawlers/migrations/0011_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('crawlers', '0004_delete_searchquery'),
+        ('crawlers', '0010_auto_20151017_0310'),
+    ]
+
+    operations = [
+    ]
