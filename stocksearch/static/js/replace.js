0a16492b96060d289b895055af37e0f2069c68d4
David Griver <dgriver@gmail.com>
2015-10-20 07:50:35 +0300

    removed adblock blocking

--- stocksearch/static/js/replace.js
+++ stocksearch/static/js/replace.js
@@ -23,5 +23,5 @@ function adBlockNotDetected() {
 
 
  if (adblock){
-   $('.download').replaceWith('<span class="button">I rely on ad-revenue to cover server costs. please disable adblocker to download photos - you will see only one ad per page. promise :)</span>');
+   $('#_carbonads_js').after('<span class="ab-msg">I rely on ad-revenue to cover server costs. please consider disabling adblocker or <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=FRUJ7J8X43DWN&lc=IL&item_name=LibreStock&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted">donating</a> - you will see only one small ad per page, promise :)</span>');
  }
